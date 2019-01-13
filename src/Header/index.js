843dbe3c436e13885d75c5787ff567a66256464a
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-13 17:11:39 +0530

    Done with Landing Slideshow

---
+++ src/Header/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import ImageOne from './Images/landing-one.png';
+import ImageTwo from './Images/landing-two.png';
+import ImageThree from './Images/landing-three.png';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <p className="test">Hi There</p>
+        <SlideShow />
+      </div>
+    );
+  }
+}
+
+class SlideShow extends Component {
+  componentDidMount() {
+    this.slideshow();
+  }
+
+  slideshow = () => {
+    $('#slideshow > div:gt(0)').hide();
+
+    setInterval(() => {
+      $('#slideshow > div:first')
+        .fadeOut(2000)
+        .next()
+        .fadeIn(2000)
+        .end()
+        .appendTo('#slideshow');
+    }, 5000);
+  };
+  render() {
+    return (
+      <div id="slideshow">
+        <div className="img-crop">
+          <img src={ImageOne} alt="Explore the World with us" />
+        </div>
+        <div className="img-crop">
+          <img src={ImageTwo} alt="Explore the World with us" />
+        </div>
+        <div className="img-crop">
+          <img src={ImageThree} alt="Explore the World with us" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
