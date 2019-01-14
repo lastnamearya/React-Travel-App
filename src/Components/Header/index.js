845d7c5ce028263ee40507930e4f46ee4ec6b9d8
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-14 06:19:27 +0530

    Searchbar implemented in Small Search Box

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
 import $ from 'jquery';
-import Searchbar from './Searchbar';
 import Logo from './Images/headout-logo.png';
 import ImageOne from '../Images/landing-one.png';
 import ImageTwo from '../Images/landing-two.png';
@@ -19,7 +19,9 @@ class Header extends Component {
                 <div>
                   <img id="logo" src={Logo} alt="Headout" />
                 </div>
-                <div className="select-city" />
+                <div className="select-city">
+                  <Searchbar style={smallSearchbar} />
+                </div>
                 <div className="select-experience" />
               </div>
               <div className="second-line">
@@ -73,8 +75,110 @@ class Header extends Component {
           </div>
         </div>
         <SlideShow />
-        <Searchbar />
+        <div className="search-bar-div">
+          <div className="select-city-large">
+            <Searchbar style={customStyles} />
+          </div>
+          <div className="select-experience-large" />
+          <button id="go">Let's Go</button>
+        </div>
       </div>
+    );
+  }
+}
+
+const options = [
+  { value: 'New York', label: 'New York' },
+  { value: 'Las Vegas', label: 'Las Vegas' },
+  { value: 'Rome', label: 'Rome' },
+  { value: 'Paris', label: 'Paris' },
+  { value: 'London', label: 'London' },
+  { value: 'Dubai', label: 'Dubai' },
+  { value: 'Barcelona', label: 'Barcelona' },
+  { value: 'Madrid', label: 'Madrid' },
+  { value: 'Singapore', label: 'Singapore' },
+  { value: 'Venice', label: 'Venice' },
+  { value: 'Milan', label: 'Milan' },
+  { value: 'Naples', label: 'Naples' },
+  { value: 'Budapest', label: 'Budapest' },
+  { value: 'Edinburg', label: 'Edinburg' },
+  { value: 'Florence', label: 'Florence' }
+];
+
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: 'none',
+    color: state.isSelected ? 'red' : '#727272',
+    padding: 10,
+    cursor: 'pointer',
+    background: state.isSelected ? 'white' : 'white',
+    fontSize: '13px',
+    textAlign: 'left',
+    paddingLeft: '20px',
+    width: 150
+  }),
+  control: () => ({
+    width: 150,
+    display: 'flex',
+    marginLeft: '30px',
+    fontSize: '14px',
+    marginTop: '10px'
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = 'opacity 300ms';
+
+    return { ...provided, opacity, transition };
+  }
+};
+
+const smallSearchbar = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: 'none',
+    color: state.isSelected ? 'red' : '#727272',
+    padding: 10,
+    cursor: 'pointer',
+    background: state.isSelected ? 'white' : 'white',
+    fontSize: '13px',
+    textAlign: 'left',
+    paddingLeft: '20px',
+    width: 150
+  }),
+  control: () => ({
+    width: 150,
+    display: 'flex',
+    fontSize: '11px',
+    marginLeft: '15px'
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = 'opacity 300ms';
+
+    return { ...provided, opacity, transition };
+  }
+};
+
+class Searchbar extends Component {
+  state = {
+    selectedOption: null
+  };
+
+  handleChange = selectedOption => {
+    this.setState({ selectedOption: selectedOption });
+  };
+
+  render() {
+    const { selectedOption } = this.state;
+    return (
+      <Select
+        styles={this.props.style}
+        placeholder="Select City"
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+      />
     );
   }
 }
