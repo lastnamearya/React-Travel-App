d05e83859eed2ff05e9764d257b2d9a5a42435c3
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-15 03:21:44 +0530

    CSS is hard

--- src/Components/TopCities.js
+++ src/Components/TopCities.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './top-cities.css';
+import LeftArrow from './Images/left-arrow.svg';
+import RightArrow from './Images/right-arrow.svg';
 
 class TopCities extends Component {
   render() {
@@ -17,10 +22,169 @@ class TopCities extends Component {
               marginLeft: '0px'
             }}
           />
+          <div className="top-cities-carousel-wrap">
+            <SimpleSlider />
+          </div>
         </div>
       </div>
     );
   }
 }
+
+class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      nextArrow: <Right />,
+      prevArrow: <Left />
+    };
+    return (
+      <Slider {...settings}>
+        {topCitiesData &&
+          topCitiesData.map(({ id, city, url }) => (
+            <CityCard key={id} city={city} url={url} />
+          ))}
+      </Slider>
+    );
+  }
+}
+
+class CityCard extends Component {
+  render() {
+    const url = `url(${this.props.url})`;
+    return (
+      <div
+        className="city-card"
+        style={{
+          backgroundImage: url
+        }}
+      >
+        <div id="triangle" />
+        <div className="city-details">
+          <p>{this.props.city}</p>
+          <p className="expose">Kuda kachra</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Left = props => (
+  <div className="city-arrow">
+    <img
+      src={LeftArrow}
+      onClick={props.onClick}
+      className="slick-prev"
+      alt="Left-arrow"
+    />
+  </div>
+);
+
+const Right = props => (
+  <div className="city-arrow">
+    <img
+      src={RightArrow}
+      onClick={props.onClick}
+      className="slick-next"
+      alt="Right-arrow"
+    />
+  </div>
+);
+
+const topCitiesData = [
+  {
+    id: 1,
+    city: 'New York',
+    url:
+      'https://cdn-imgix.headout.com/cities/new-york/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 2,
+    city: 'Las Vegas',
+    url:
+      'https://cdn-imgix.headout.com/cities/las-vegas/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 3,
+    city: 'Rome',
+    url:
+      'https://cdn-imgix.headout.com/cities/rome/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 4,
+    city: 'Paris',
+    url:
+      'https://cdn-imgix.headout.com/cities/paris/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 5,
+    city: 'London',
+    url:
+      'https://cdn-imgix.headout.com/cities/london/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 6,
+    city: 'Dubai',
+    url:
+      'https://cdn-imgix.headout.com/cities/dubai/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 7,
+    city: 'Barcelona',
+    url:
+      'https://cdn-imgix.headout.com/cities/barcelona/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 8,
+    city: 'Madrid',
+    url:
+      'https://cdn-imgix.headout.com/cities/madrid/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 9,
+    city: 'Singapore',
+    url:
+      'https://cdn-imgix.headout.com/cities/singapore/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 10,
+    city: 'Venice',
+    url:
+      'https://cdn-imgix.headout.com/cities/venice/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 11,
+    city: 'Milan',
+    url:
+      'https://cdn-imgix.headout.com/cities/milan/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 12,
+    city: 'Naples',
+    url:
+      'https://cdn-imgix.headout.com/cities/naples/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 13,
+    city: 'Budapest',
+    url:
+      'https://cdn-imgix.headout.com/cities/budapest/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 14,
+    city: 'Edinburg',
+    url:
+      'https://cdn-imgix.headout.com/cities/edinburgh/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  },
+  {
+    id: 15,
+    city: 'Florence',
+    url:
+      'https://cdn-imgix.headout.com/cities/florence/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
+  }
+];
 
 export default TopCities;
