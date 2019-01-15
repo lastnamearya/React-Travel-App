5ff0e071c43171dc21be93eb2a86af587969084e
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-15 16:45:58 +0530

    Done with Top Cities

--- src/Components/TopCities.js
+++ src/Components/TopCities.js
@@ -52,8 +52,13 @@ class SimpleSlider extends React.Component {
     return (
       <Slider {...settings}>
         {topCitiesData &&
-          topCitiesData.map(({ id, city, url }) => (
-            <CityCard key={id} city={city} url={url} />
+          topCitiesData.map(({ id, city, url, description }) => (
+            <CityCard
+              key={id}
+              city={city}
+              url={url}
+              description={description}
+            />
           ))}
       </Slider>
     );
@@ -65,17 +70,18 @@ class CityCard extends Component {
     const url = `url(${this.props.url})`;
     return (
       <div className="city-card-wrapper">
-        <div
-          className="city-card"
-          style={{
-            backgroundImage: url
-          }}
-        >
-          <div id="triangle" />
+        <div className="city-card">
+          <div
+            className="city-card-img"
+            style={{
+              backgroundImage: url
+            }}
+          />
           <div className="city-details">
+            <div id="triangle" />
             <p>{this.props.city}</p>
-            <div className="city-hidden">
-              <p>Kuda kachra</p>
+            <div id="city-hidden">
+              <p>{this.props.description}</p>
             </div>
           </div>
         </div>
@@ -110,54 +116,63 @@ const topCitiesData = [
   {
     id: 1,
     city: 'New York',
+    description: 'Take a bite of the Big Apple',
     url:
       'https://cdn-imgix.headout.com/cities/new-york/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 2,
     city: 'Las Vegas',
+    description: "An offer you can't refuse",
     url:
       'https://cdn-imgix.headout.com/cities/las-vegas/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 3,
     city: 'Rome',
+    description: 'Roam the eternal city',
     url:
       'https://cdn-imgix.headout.com/cities/rome/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 4,
     city: 'Paris',
+    description: "C'est La Vie",
     url:
       'https://cdn-imgix.headout.com/cities/paris/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 5,
     city: 'London',
+    description: 'For everything hunky-dory',
     url:
       'https://cdn-imgix.headout.com/cities/london/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 6,
     city: 'Dubai',
+    description: 'An Oasis like no other',
     url:
       'https://cdn-imgix.headout.com/cities/dubai/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 7,
     city: 'Barcelona',
+    description: 'Hacer Peunte a Catalunya',
     url:
       'https://cdn-imgix.headout.com/cities/barcelona/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 8,
     city: 'Madrid',
+    description: 'Discover the hear of Spain',
     url:
       'https://cdn-imgix.headout.com/cities/madrid/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
   {
     id: 9,
     city: 'Singapore',
+    description: 'The Lion City',
     url:
       'https://cdn-imgix.headout.com/cities/singapore/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min'
   },
