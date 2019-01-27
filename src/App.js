8c420ebec63359bbe3ebb7e414086f0c5d53362a
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-27 15:25:00 +0530

    Mobile/Tablet Screen Placeholder moved to Main App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 import Routes from './CityRoutes';
+import logo from './Components/Images/headout.png';
 
 class App extends Component {
+  state = { width: 0, height: 0 };
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  updateWindowDimensions = () =>
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+
   render() {
-    return <Routes />;
+    const { width } = this.state;
+    const mobileTablet = width <= 1100;
+
+    if (mobileTablet) {
+      return (
+        <div className="mobile-tablet">
+          <img src={logo} alt="Headout" />
+          <p>
+            Currently, we're not supporting Mobile & Tablets{' '}
+            <span role="img" aria-label="Warn">
+              🙏
+            </span>
+          </p>
+        </div>
+      );
+    } else {
+      return <Routes />;
+    }
   }
 }
 
--- src/MainHome.js
+++ src/MainHome.js
@@ -6,61 +6,33 @@ import Collections from './Components/Collections';
 import Download from './Components/Download';
 import Media from './Components/Media';
 import Footer from './Components/footer';
-import logo from './Components/Images/headout.png';
 import './main-home.css';
 
 class MainHome extends Component {
-  state = { width: 0, height: 0 };
-
   componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
+    window.scrollTo(0, 0);
   }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-  updateWindowDimensions = () =>
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
   render() {
-    const { width } = this.state;
-    // const mobileTablet = width <= 1100;
-    const mobileTablet = undefined;
-
-    if (mobileTablet) {
-      return (
-        <div className="mobile-tablet">
-          <img src={logo} alt="Headout" />
-          <p>
-            Currently, we're not supporting Mobile & Tablets{' '}
-            <span role="img" aria-label="Warn">
-              🙏
-            </span>
-          </p>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <Header backgroundImagesData={backgroundImagesData} />
-          <p id="external">
-            <span role="img" aria-label="Warn">
-              🙏
-            </span>{' '}
-            No any External Link is added as per now to anywhere, Please bear{' '}
-            <span role="img" aria-label="Warn">
-              🙏
-            </span>
-          </p>
-          <TopCities />
-          <HeadoutPicks pickedData={pickedData} headline={'Headout Picks'} />
-          <Collections collectionsData={collectionsData} />
-          <Download />
-          <Media />
-          <Footer />
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        <Header backgroundImagesData={backgroundImagesData} />
+        <p id="external">
+          <span role="img" aria-label="Warn">
+            🙏
+          </span>{' '}
+          No any External Link is added as per now to anywhere, Please bear{' '}
+          <span role="img" aria-label="Warn">
+            🙏
+          </span>
+        </p>
+        <TopCities />
+        <HeadoutPicks pickedData={pickedData} headline={'Headout Picks'} />
+        <Collections collectionsData={collectionsData} />
+        <Download />
+        <Media />
+        <Footer />
+      </div>
+    );
   }
 }
 
